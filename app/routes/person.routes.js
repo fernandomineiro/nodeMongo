f4fcc8326b3b2fcb53097d3a04eefa5d0f38a117
fernando fernandes <fernandofitilan@hotmail.com>
2022-08-24 17:08:00 -0300

    subindo o back

---
+++ app/routes/person.routes.js
@@ -0,0 +1,19 @@
+module.exports = (app) => {
+  const person = require("../controllers/person.controller.js");
+
+  var router = require("express").Router();
+
+  router.post("/", person.create);
+
+  router.get("/", person.findAll);
+
+  router.get("/:id", person.findOne);
+
+  router.put("/:id", person.update);
+
+  router.delete("/:id", person.delete);
+
+  router.delete("/", person.deleteAll);
+
+  app.use("/api/person", router);
+};
